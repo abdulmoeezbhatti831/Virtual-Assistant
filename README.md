255b2fa4394be26d71808a0c0f44171dcb426545
abdulmoeezbhatti831 <amb91309@gmail.com>
2025-09-08 16:37:46 +0500

    Update README and refactor code for improved clarity and functionality

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Virtual Assistant
+# Virtual Assistant (Jarvis / Friday)
+
 Author: Abdul Moeez Bhatti
-- Created a Virtual Assistant with voice recognition and various commands
+
+- This is a customizable voice-powered Virtual Assistant built in Python that helps automate daily tasks with simple voice commands. It can open apps like Google, YouTube, Camera, perform Google/Wikipedia searches, play music, send WhatsApp messages & emails, control files/folders, and even run commands in the terminal.
+
+- The assistant is fully interactive with speech recognition and text-to-speech responses, making the experience feel conversational. It also comes with a fun loading animation, chatbot integration, and exciting programs to explore.
+
+- Whether you want to boost productivity, automate repetitive actions, or just have some fun with your own AI assistant, this project is designed to be both useful and enjoyable.
--- Virtual Assistant.py
+++ Virtual Assistant.py
@@ -1,7 +1,7 @@
-from win32com.client import Dispatch
-from colorama import init, Fore, Style, Back
+from win32com.client import Dispatch # pip install pywin32
+from colorama import init, Fore, Style, Back 
 import pywhatkit as kit
-import speech_recognition as sr
+import speech_recognition as sr # pip install speechRecognition
 import datetime
 import os
 import shutil
@@ -12,8 +12,10 @@ import wikipedia
 import threading
 import random
 
+# Autoreset the console color after each print function
 init(autoreset=True)
 
+# PC voice / speaking by PC
 def speak(argu: str):
     global name
     speak = Dispatch("SAPI.SpVoice")
@@ -23,7 +25,7 @@ def speak(argu: str):
     else:
         speak.Speak(argu)
 
-
+# Loading Animation
 def loading(word: str, time_sec: int):
     spinner = ["|", "/", "-", "\\"]
     x = 0
@@ -38,27 +40,29 @@ def loading(word: str, time_sec: int):
         time.sleep(0.1)
         x += 1
 
-
+# Welcome Screen
 def welcome():
     global name
     name = input(Style.BRIGHT + Fore.BLUE + "Name for your Virtual Assistant (Jarvis / Friday) > " + Style.RESET_ALL).lower()
+    # Automatic name selection if user didn't enter or give other then given ones
     if name == "" or not any(name == voice for voice in ["jarvis", "friday"]): name = random.choice(["jarvis", "friday"])
     
+    # Welcome acccording to current time
     hour = int(datetime.datetime.now().hour)
-    if 4 <= hour < 12:
+    if 4 <= hour < 12: 
         speak("Good Morning!")
     elif 12 <= hour < 18:
         speak("Good Afternoon!")
     elif 18 <= hour < 22:
         speak("Good Evening!")
-    else:
+    else: # If it's quite night then program exit as Parents said to sleep quickly
         speak("It's quite late at night! You should get some rest. Good night and have sweet dreams! Take care!")
         exit(0)
 
     speak(f"Hello! I am {name.title()}, your Virtual Assistant, ready to help you with a variety of tasks.")
     speak("Just tell me what you need, and I'll do my best to assist you!")
 
-
+# Use voice command to take input from the user
 def user_command():
     global name
     r = sr.Recognizer()
@@ -73,11 +77,12 @@ def user_command():
         time.sleep(2)
         return user_said
 
-
+# Main Body
 if __name__ == "__main__":
     name = "Jarvis"
     welcome()
 
+    # Menu
     print(Style.BRIGHT + Fore.CYAN + "\n✨ Here are some things I can do for you:")
     print(      Fore.YELLOW + """
     ┌─────────────────────────────────────────────────────────────┐
@@ -104,42 +109,51 @@ if __name__ == "__main__":
     print(Style.RESET_ALL)
 
     loading("⌚ Wait a while", 5)
-    speak("Press enter, To EXPLORE me further!")
+    speak("Press Enter, To EXPLORE me further!")
     input()
         
-        
+    # While Loop to give infinite command
     while True:
         try:
+            # Taking the user input as query
             query = user_command().lower()
 
+            # Open Camera
             if all(aciton in query for aciton in ["open", "camera"]):
                 speak("Opening Camera..., Now give a sweet look!")
                 os.system("start microsoft.windows.camera:")
                 
+            # Open Google
             elif all(aciton in query for aciton in ["open", "google"]):
                 speak("Going to WORLD's number one SEARCH ENGINE 'GOOGLE'!!!")
                 webbrowser.open("https://www.google.com")
                 
+            # Open Youtube
             elif all(aciton in query for aciton in ["open", "youtube"]):
                 speak("Lets explore the WORLD's Most popular Social Platform, YOUTUBE...")
                 webbrowser.open("https://www.youtube.com")
                 
+            # Google Search
             elif all(aciton in query for aciton in ["google", "search"]):
                 speak("Type to take a look GOOGLE!")
                 kit.search(input(Style.BRIGHT + Fore.CYAN + "Search on Google Here: " + Style.RESET_ALL))
                 
+            # Youtube Search
             elif all(aciton in query for aciton in ["youtube", "search"]):
                 speak("Just type and get a video on it on YOUTUBE!")
                 webbrowser.open(f"https://www.youtube.com/results?search_query={input(Style.BRIGHT + Fore.CYAN + "Search on YouTube Here: " + Style.RESET_ALL).replace(" ", "+")}")
                 
+            # Open Website through URL
             elif all(aciton in query for aciton in ["website", "url"]):
                 speak("Give me the URL and I'll search upon it...")
                 webbrowser.open(input(Style.BRIGHT + Fore.CYAN + "Website URL: " + Style.RESET_ALL))
                 
+            # Date and Time
             elif any(aciton in query for aciton in ["date", "time"]):
                 speak("Okay, I also wanted to know the Date and Time...")
                 print(Fore.LIGHTGREEN_EX + datetime.datetime.now().strftime("Current Date and Time:\n\nDate: %B %d, %Y.\nDay: %A\nTime: %I:%M:%S %p"))
             
+            # Wikipedia Search
             elif all(aciton in query for aciton in ["wikipedia", "search"]):
                 speak("Get information on anything here. Just type the TOPIC...")
                 result = wikipedia.summary(input(Style.BRIGHT + Fore.CYAN + "Topic: " + Style.RESET_ALL), sentences=input(Fore.CYAN + "No. of sentences: " + Style.RESET_ALL))
@@ -148,13 +162,16 @@ if __name__ == "__main__":
                 time.sleep(3)
                 speak(result)
                 
+            # CMD Runner
             elif all(aciton in query for aciton in ["cmd", "run"]):
                 speak("CMD initiating...")
                 os.system(input(Style.BRIGHT + Fore.CYAN + "Command: " + Style.RESET_ALL))
                 
+            # Writing a note + saving 
             elif all(aciton in query for aciton in ["write", "note"]):
                 speak("Making you ready the environment...")
                 
+                # Function for writing
                 def write_note():
                     speak("Write or speak?")
                     user = input(Style.BRIGHT + Fore.CYAN + "> " + Style.RESET_ALL).lower()
@@ -184,12 +201,16 @@ if __name__ == "__main__":
                         speak(f"Sorry, {user} is not available!")
                         return write_note()
                 
+                # Loop for if given path already exists
                 while True: 
                     Path = input(Style.BRIGHT + Fore.CYAN + "Path: " + Style.RESET_ALL)
                     note = input(Fore.CYAN + "Create Text File: " + Style.RESET_ALL)
+                    
+                    # Making sure to create .txt format
                     if not note.endswith(".txt"): note = f"{Path}\\{note}.txt"
                     else: note = f"{Path}\\{note}"
 
+                    # Verfying the given path
                     if not os.path.exists(note):
                         with open(note, "w") as f:
                             f.write(write_note())
@@ -199,11 +220,12 @@ if __name__ == "__main__":
                     else:
                         speak("Such name already have used try another!")
                     
-                        
+            # Play Music (.mp3) anywhere in your PC
             elif all(aciton in query for aciton in ["play", "music"]):
                 speak("🎶 It's Music Time! Let's get the party started!")
                 speak("First, let's create a list of songs you want to play. Type 'exit' at the end when you're done.")
                 
+                # Creating the list 
                 print(Fore.MAGENTA + "🎵 Create the list: ")
                 songs = []
                 while True:
@@ -212,13 +234,14 @@ if __name__ == "__main__":
                         break
                     songs.append(song)
                     
-                found = False
+                found = False # Initially found is false
                 
+                # Finding Animation
                 def search_animation():
                     x = 1
                     i = 0
                     while True:
-                        if found:
+                        if found: # break when song found
                             break
                         if i % 3 == 0:
                             sys.stdout.write(Fore.BLUE + "\r🔎 Finding Your Songs    ")
@@ -229,6 +252,7 @@ if __name__ == "__main__":
                         x += 1
                         i += 1
                     
+                # Searching for each song in the list from entire PC
                 for song in songs:
                     t = threading.Thread(target=search_animation)
                     t.start()
@@ -236,7 +260,7 @@ if __name__ == "__main__":
                         for root, dir, files in os.walk(drive):
                             for file in files:
                                 if (song.lower() in file.lower() or song.lower().replace(" ", "_") in file.lower()) and file.endswith(".mp3"):
-                                    found = True
+                                    found = True # If found then play and break
                                     t.join()
                                     print(Style.BRIGHT + "\nSong Found: " + Back.LIGHTBLUE_EX + os.path.join(root, file))
                                     os.startfile(os.path.join(root, file))
@@ -245,21 +269,23 @@ if __name__ == "__main__":
                                 break
                         if found:
                             break
-                    if not found:
-                        found = True
+                    if not found: # if not found then will go for the next one
+                        found = True # true for breaking the search animation
                         t.join()
                         print(Fore.RED + f"\n❌ {song} Not Found!\n")
-                    found = False
+                    found = False # Reset to false
                 print(Fore.GREEN + "✅ Enjoy!\n")
 
-            
+            # Just Open Whatsapp -> Done through web whatsapp
             elif all(aciton in query for aciton in ["just", "open", "whatsapp"]):
                 speak("Just ensure your WhatsApp is connected to WhatsApp Web in your browser. Opening WhatsApp Web for you now!")
                 kit.open_web()
                 
+            # Whatsapp automated messaging -> Make sure to not use it much. As to avoid from spamming which can result suspend account
             elif all(aciton in query for aciton in ["whatsapp", "message"]):
                 speak("Oh YES! It's CHIT CHAT TIME! Let's send a WhatsApp message. Make sure your WhatsApp Web is connected.")
                 
+                # Taking messages
                 def msg():
                     speak("Write or speak?")
                     user = input("> ").lower()
@@ -290,37 +316,50 @@ if __name__ == "__main__":
                         return msg()
                 
                 receiver = input(Style.BRIGHT + Fore.YELLOW + "Receiver Phone No. or Group ID: " + Style.RESET_ALL)
+                
+                # If receiver is a Group ID in which user is joined and able to send msg
                 if any(char.lower() in "abcdefghijklmnopqrstuvwxyz" for char in receiver):
                     kit.sendwhatmsg_to_group_instantly(receiver, msg(), tab_close=True)
                     print(Fore.GREEN + "✅ Task Done!")
+                # If receiver is a Phone no.
                 else:
                     kit.sendwhatmsg_instantly(receiver, msg(), tab_close=True)
                     print(Fore.GREEN + "✅ Task Done!")
                     
+            # Sending Email
             elif all(aciton in query for aciton in ["send", "mail"]):
-                speak("Make sure your message should well written! And one thing more here you need your email app pass not original password! If don't know search about it!")
+                speak("Make sure your message should well written!")
                 EMAIL_ID = input(Style.BRIGHT + Fore.CYAN + "Your Email ID: " + Style.RESET_ALL)
-                EMAIL_PWD = input(Fore.CYAN + "Your Apps Password: " + Style.RESET_ALL)
+                EMAIL_PWD = input(Fore.CYAN + "Your Apps Password: " + Style.RESET_ALL) # Here you need your email account app pass not original password! If don't know search about it!
                 EMAIL_RECEIVER = input(Fore.CYAN + "Receiver Email ID: " + Style.RESET_ALL)
                 kit.send_mail(EMAIL_ID, EMAIL_PWD, input(Style.BRIGHT + "Subject: " + Style.RESET_ALL), input(Style.BRIGHT + "Message: " + Style.RESET_ALL), EMAIL_RECEIVER)
                     
+            # Chat Bot -> Chat GPT
             elif all(aciton in query for aciton in ["chat", "bot"]):
                 speak("Okay, go to do some chat with a real-time AI!")
                 webbrowser.open("https://www.chatgpt.com")
                 
+            # Controlling Files and Folder in your PC
             elif "control" in query and any(aciton in query for aciton in ["file", "folder"]):
                 speak("Ready to manage your files and folders! Let's Control your system!")
                 print(Fore.LIGHTYELLOW_EX + "↪ Create  ↪ Delete  ↪ Move  ↪ Rename  ↪ Copy  ↪ Compress  ↪ List")
                 
+                # Current Directory Path
                 OG_Path = os.getcwd()
                 
+                # Setting the Path in which actions are to be perfromed
                 speak("Make sure to set the path First!")
                 Path = input(Style.BRIGHT + Fore.GREEN + "Set the Path where to perform the action: " + Style.RESET_ALL)
+                
+                # if Path exists
                 if os.path.exists(Path):
                     speak("PATH EXIST!! Access Granted!")
                     print(Fore.GREEN + "✅ Access Granted!")
+                    
+                    # Changing current directory to this path to perform actions more easily
                     os.chdir(Path)
                     
+                    # Loop for many commands one after one
                     while True:
                         command = input(Fore.YELLOW + "\nSo, tell me what to do -> " + Style.RESET_ALL).lower()
                         
@@ -395,25 +434,31 @@ if __name__ == "__main__":
                                 speak(f"Sorry, {command} is not available!")
                                 continue
                         
+                        # Handling any error while controlling files and folders
                         except Exception as e:
                             speak("Path File or Folder ERROR!")
                             print(Fore.MAGENTA + Style.BRIGHT + "\n[Error]: " + Style.NORMAL + str(e))
 
+                        # Asking for more actions or done actions
                         if input(Fore.BLUE + Style.BRIGHT + "\nMore actions in this Path (y/n)? > " + Style.RESET_ALL).lower() == "y":
                             continue
                         else:
                             break
                         
+                    # Get back to current working directory
                     os.chdir(OG_Path)
                         
+                # If path not exists
                 else:
                     speak("Sorry, I can't perform the action as the path you have provided doesn't exit!")
                     print(Fore.RED + "❌ Path doesn't exists!")
                     
+            # Exciting Programs of the Coder
             elif all(aciton in query for aciton in ["excit", "program"]):
                 speak("Let me show you, the exciting programs, ever created, learnt, or had, by MY CODER!!")
                 webbrowser.open("https://www.github.com/abdulmoeezbhatti831")
                 
+            # For Exiting the program or this Virtual Assistant
             elif all(aciton in query for aciton in ["exit", "program"]):
                 speak("Thank you for choosing me as your Virtual Assistant! I hope I was able to help you today. Have a wonderful day ahead!")
                 speak("Exiting now. In:")
@@ -423,8 +468,11 @@ if __name__ == "__main__":
                 print(Fore.RED + "❌ Exit!")
                 break
                 
+            # if query doesn't match any of all the above ones
             else:
                 speak("Sorry, I couldn't recognize your command. Please try again with one of the listed options or rephrase your request.")
+                
+                # Again dislaying the Menu
                 print(Style.BRIGHT + Fore.CYAN + "\n✨ Here are some things I can do for you:")
                 print(Fore.YELLOW + """
     ┌─────────────────────────────────────────────────────────────┐
@@ -450,11 +498,14 @@ if __name__ == "__main__":
                 """)
                 print(Style.RESET_ALL)
                 
+        # Handling "if user said nothing" error
         except sr.UnknownValueError:
             speak("Hmmmmm, I got you, you didn't say anything!!")
             
+        # Handling any other remaining error 
         except Exception as e:
             speak("It looks like we are facing an ERROR!")
             print(Fore.MAGENTA + Style.BRIGHT + "\n[Error]: " + Style.NORMAL + str(e))
             
+        # Gap to next query / for not running continously / if user leaves the program ON
         input(Style.DIM + "\nPress Enter to continue...\n" + Style.RESET_ALL)
