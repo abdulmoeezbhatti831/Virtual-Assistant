fc2b10c5bbea66fd2cfcec9e4443bda018e65a89
Abdul Moeez Bhatti <amb91309@gmail.com>
2025-09-27 11:25:42 +0500

    Update chat initialization message for clarity and enhance user instructions

--- Virtual Assistant.py
+++ Virtual Assistant.py
@@ -378,9 +378,9 @@ if __name__ == "__main__":
                         return user_said
                     
                 # Setting the Parameters
-                chat = ""
+                chat = f"Saved in Memory: 'You're name is {name.title()}! An Ai assistant like created by Tony Stark, THE IRON MAN!!\nThis is a voice-chat program with you!'\n\n"
                 conversation_done = False
-                speak("Start chatting! Say something to Start Chatting. You can say 'exit' to stop chatting.")
+                speak("Start chatting! Say something to Start Chatting. You can say just only 'exit' to stop chatting.")
                 
                 # Loop to chat continuously
                 while True:
